44833aaf9909d7ea465aff740a938b6aa64b65fc
Sean Murphy <smurphy@in4ins.com>
2019-04-15 12:00:28 -0400

    making sim_mat remain accross iterations until refreshed.
    sim_mat is now a reactive value which updates with operations rather than a reactive object which will call back through chains

--- server.R
+++ server.R
@@ -6,24 +6,35 @@ shinyServer(function(input, output) {
   colorkey <- list("X" = "Black",
                    "O" = "White")
   
-  values <- reactiveValues()
-  sim_mat <- refresh_sim()
+  sim_mat <- reactiveVal()
+  clean_sim <- refresh_sim()
+  sim_mat(clean_sim) 
+  
+  
+  # values <- reactiveValues()
+  # sim_mat <- refresh_sim()
+  
+  
+  
   output$hexplot <- renderPlot({
-    hexplot(sim_mat, colorkey)
+    hexplot(sim_mat(), colorkey)
   }) 
   
-  observeEvent(input$regen_hex, {
-    sim_mat <- refresh_sim()
-    output$hexplot <- renderPlot({
-      hexplot(sim_mat, colorkey)
-    }) 
+  isolate({
+    observeEvent(input$regen_hex, {
+      clean_sim <- refresh_sim()
+      sim_mat(clean_sim) 
+      output$hexplot <- renderPlot({
+        hexplot(sim_mat(), colorkey)
+      })
+    })
   })
-    
+  
   observeEvent(input$self_play, {
-    sim_mat <- self_play(isolate(sim_mat))
-    
+    sp_mat <- self_play(sim_mat())
+    sim_mat(sp_mat)
     output$hexplot <- renderPlot({
-      hexplot(sim_mat, colorkey)
+      hexplot(sim_mat(), colorkey)
     }) 
   })
   
